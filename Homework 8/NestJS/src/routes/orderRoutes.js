d856d9b669521f73fe1b524c8390b5927f7d2158
IvanGodPro24 <144848479+IvanGodPro24@users.noreply.github.com>
2023-12-05 12:20:47 +0200

    Nest project deleted

--- Homework 8/NestJS/src/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/OrderController');
-
-router.delete('/:orderId', orderController.deleteOrder);
-
-module.exports = router;
